d1d29c1bcd2e336e4fb9a981855d2bce1c012d2c
Kent <dylankentish@live.co.uk>
2023-02-13 23:19:28 +0000

    basic wallet-connect
    - custom button
    - wagmi hook contract interaction

---
+++ src/main.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+
+import { Web3Modal } from "@web3modal/react";
+
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+import { mainnet } from "wagmi/chains";
+
+const network = mainnet;
+
+const { chains, provider, webSocketProvider } = configureChains(
+	[ network ],
+	[
+    alchemyProvider({
+			apiKey: import.meta.env.VITE_ALCHEMY_API_KEY,
+			priority: 0,
+			weight: 1
+		}),
+		publicProvider({ priority: 1, weight: 2 })
+	]
+);
+
+const client = createClient({
+	autoConnect: true,
+	connectors: modalConnectors({ appName: 'web3Modal', chains }),
+	provider,
+	webSocketProvider
+});
+
+// Web3Modal Ethereum Client
+const ethereumClient = new EthereumClient(client, chains);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <>
+      <WagmiConfig client={client}>
+        <App />
+      </WagmiConfig>
+
+      <Web3Modal
+        projectId={import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID}
+        ethereumClient={ethereumClient}
+      />
+    </>
+  </React.StrictMode>,
+)
